0b1d7d2b5e1e91a36cab61a9dce73876122cc39e
bedialp <fvzlpktk@gmail.com>
2024-04-26 23:42:23 +0300

    Repositories, Admin Layout Added

---
+++ MvcCv/Controllers/HakkimdaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+
+namespace MvcCv.Controllers
+{
+    public class HakkimdaController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
