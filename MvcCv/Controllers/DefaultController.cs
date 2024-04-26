31cb9557465e6851ec07268664ce1074ef7ff413
bedialp <fvzlpktk@gmail.com>
2024-04-26 03:42:52 +0300

    Vieews

--- MvcCv/Controllers/DefaultController.cs
+++ MvcCv/Controllers/DefaultController.cs
@@ -10,14 +10,55 @@ namespace MvcCv.Controllers
     public class DefaultController : Controller
     {
         DbCvEntities db = new DbCvEntities();
+
         public ActionResult Index()
         {
             var degerler = db.TblHakkimda.ToList();
             return View(degerler);
         }
-
+        // DENEYIM
         public PartialViewResult Deneyim()
         {
+            var deneyimler = db.TblDeneyimlerim.ToList();
+            return PartialView(deneyimler);
+        }
+        // EGITIMLERIM
+        public PartialViewResult Egitimlerim()
+        {
+            var egitimler = db.TblEgitimlerim.ToList();
+            return PartialView(egitimler);
+        }
+        // YETENEKLERIM
+        public PartialViewResult Yeteneklerim()
+        {
+            var yetenekler = db.TblYeteneklerim.ToList();
+            return PartialView(yetenekler);
+        }
+        // HOBILERIM
+        public PartialViewResult Hobilerim()
+        {
+            var hobiler = db.TblHobilerim.ToList();
+            return PartialView(hobiler);
+        }
+        // SERTIFIKALARIM
+        public PartialViewResult Sertifikalarim()
+        {
+            var sertifikalar = db.TblSertifikalarim.ToList();
+            return PartialView(sertifikalar);
+        }
+        // ILETISIM
+        [HttpGet]
+        public PartialViewResult Iletisim()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult Iletisim(TblIletisim iletisim)
+        {
+            iletisim.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.TblIletisim.Add(iletisim);
+            db.SaveChanges();
             return PartialView();
         }
     }
