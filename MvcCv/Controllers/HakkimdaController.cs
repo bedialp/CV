08d8e770cf2374873c887e4d505f45b133a4b6ef
bedialp <fvzlpktk@gmail.com>
2024-04-28 20:55:01 +0300

    Hakkimda ~

--- MvcCv/Controllers/HakkimdaController.cs
+++ MvcCv/Controllers/HakkimdaController.cs
@@ -4,15 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MvcCv.Models.Entity;
+using MvcCv.Repositories;
 
 namespace MvcCv.Controllers
 {
     public class HakkimdaController : Controller
     {
-        
+        GenericRepository<TblHakkimda> repo = new GenericRepository<TblHakkimda>();
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            var hakkimda = repo.List();
+            return View(hakkimda);
+        }
+        [HttpPost]
+        public ActionResult Index(TblHakkimda h)
+        {
+            var t = repo.Find(x => x.ID == 1);
+            t.Ad = h.Ad;
+            t.Soyad = h.Soyad;
+            t.Adres = h.Adres;
+            t.Mail = h.Mail;
+            t.Telefon = h.Telefon;
+            t.Aciklama = h.Aciklama;
+            t.Resim = h.Resim;
+            repo.TUpdate(t);
+            return RedirectToAction("Index");
         }
     }
 }
